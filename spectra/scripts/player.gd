6f7b0ebdfab8df3b817c8ce78b5a4bb63152decf
Marcel Blasius <blasius.marcel@gmail.com>
2026-02-01 00:21:50 +0100

    player bullets always visible and change color based on location

--- spectra/scripts/player.gd
+++ spectra/scripts/player.gd
@@ -2,8 +2,7 @@ extends CharacterBody2D
 
 @export var health = 3
 @export var speed = 400
-const sun_bullet_scene = preload("res://scenes/sun_bullet.tscn")
-const dark_bullet_scene = preload("res://scenes/dark_bullet.tscn")
+const bullet_scene = preload("res://scenes/player_bullet.tscn")
 
 @export var fire_rate: float = 0.5 # seconds
 @onready var shoot_timer = $ShootTimer
@@ -129,13 +128,7 @@ func play_shoot_animation(dir: Vector2):
 	recoil_player2.play("shoot_animation")
 
 func spawn_bullet(dir: Vector2, offset: Vector2):
-	var world = get_node("/root/main_scene/AlphaContainer/AlphaView").get_world(self.global_position)
-
-	var bullet: Node
-	if (world == 0):
-		bullet = sun_bullet_scene.instantiate()
-	else :
-		bullet = dark_bullet_scene.instantiate()
+	var bullet = bullet_scene.instantiate()
 		
 	play_shoot_animation(dir)
 	bullet.global_position = global_position + (offset / 2)
---
+++ spectra/scripts/player_bullet.gd
@@ -0,0 +1,83 @@
+extends Area2D
+
+@export var speed: float = 600.0
+@export var damage: int = 1
+@export var hit_group : String
+
+const explosion = preload("res://scenes/explosion.tscn")
+
+var direction: Vector2 = Vector2.ZERO
+
+var view1 : Node;
+var view2 : Node;
+var spriteParent1 : Node;
+var spriteParent2 : Node;
+var sprite1 : AnimatedSprite2D
+var sprite2 : AnimatedSprite2D
+
+func _ready() -> void:
+	view1 = get_node("/root/main_scene/Subview1")
+	view2 = get_node("/root/main_scene/Subview2")
+	spriteParent1 = $World1Bullet
+	spriteParent2 = $World2Bullet
+	
+	sprite1 = $World1Bullet/SunBullet
+	sprite2 = $World2Bullet/DarkBullet
+	sprite1.z_index = 1000
+	sprite2.z_index = 1000
+	
+	var old_global1 := spriteParent1.global_transform as Transform2D
+
+	spriteParent1.get_parent().remove_child(spriteParent1)
+	view1.add_child(spriteParent1)
+	spriteParent1.global_transform = old_global1
+	
+	var old_global2 := spriteParent2.global_transform as Transform2D
+
+	spriteParent2.get_parent().remove_child(spriteParent2)
+	view2.add_child(spriteParent2)
+	spriteParent2.global_transform = old_global2
+	
+	spriteParent1.visible = false
+	spriteParent2.visible = false
+
+func _process(delta: float) -> void:
+	spriteParent1.global_transform = global_transform
+	spriteParent2.global_transform = global_transform
+	
+func _physics_process(delta: float) -> void:
+	
+	position += direction * speed * delta
+	
+	var world = get_node("/root/main_scene/AlphaContainer/AlphaView").get_world(self.global_position)
+	if world == 0:
+		spriteParent1.visible = true
+		spriteParent2.visible = false
+	else:
+		spriteParent1.visible = false
+		spriteParent2.visible = true
+	
+func destroy(body: Node2D):
+	
+	if body.is_in_group("environment"):
+		var splat : GPUParticles2D = explosion.instantiate()
+
+		var bounce_direction = -direction.normalized()
+		splat.global_position = self.global_position
+		splat.look_at(splat.global_position + bounce_direction)#
+		splat.emitting = true
+		get_tree().current_scene.add_child(splat)
+	
+	queue_free()
+	spriteParent1.queue_free()
+	spriteParent2.queue_free()
+	
+func _on_body_entered(body: Node2D):
+	
+	if !body.is_in_group(hit_group) && !body.is_in_group("environment"):
+		return
+		
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	
+	destroy(body)
