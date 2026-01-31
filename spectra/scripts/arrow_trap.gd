5ae3bb8a9813a05ab7bd15463ff3c9da82ffda34
Marcel Blasius <blasius.marcel@gmail.com>
2026-01-31 17:22:58 +0100

    arrow trap

--- spectra/scripts/arrow_trap.gd
+++ spectra/scripts/arrow_trap.gd
@@ -1,14 +1,44 @@
-extends StaticBody2D
+extends CharacterBody2D
 
 const bullet_scene = preload("res://scenes/bullet.tscn")
 
+@export var movement_speed: float = 50
 @export var fire_rate: float = 0.5 # seconds
 @onready var shoot_timer = $ShootTimer
+@onready var rotaiton_cooldown = $RotationCooldown
 @onready var sprite = $Sprite2D
 
+var direction: Vector2 
+var last_collision : int
+var before_last_collision: int
 
-
+func cooldown_collider():
+	var own_collider = $CollisionPolygon2D
+	own_collider.disabled = true
+	await get_tree().create_timer(1.5).timeout
+	own_collider.disabled = false
+	
+func move():
+	var collided = move_and_slide()
+	
+	if collided && rotaiton_cooldown.is_stopped():
+		var collision = get_last_slide_collision()
+		var collider = collision.get_collider()
+		
+		if collider.is_in_group("environment"):
+			var normal = collision.get_normal()
+			global_position += normal * 2.0
+			direction = direction.orthogonal()
+			rotaiton_cooldown.start(3)
+			cooldown_collider()
+		
+			self.rotate(-deg_to_rad(90))
+		
+	velocity = direction * movement_speed
+	
 func _physics_process(_delta: float):	
+	move()
+	
 	if !shoot_timer.is_stopped(): return
 		
 	var size = sprite.get_rect().size * sprite.scale
@@ -16,6 +46,7 @@ func _physics_process(_delta: float):
 	
 	spawn_bullet(-transform.y, -offset)
 	shoot_timer.start(fire_rate)
+	
 	
 func spawn_bullet(dir: Vector2, offset: Vector2):
 	var bullet = bullet_scene.instantiate()
@@ -27,5 +58,5 @@ func spawn_bullet(dir: Vector2, offset: Vector2):
 
 
 func _on_ready() -> void:
-	# initial delay
+	direction = transform.x
 	shoot_timer.start(randf_range(2.0, 5.0))
