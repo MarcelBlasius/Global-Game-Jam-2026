daba7e61fef97b85259ea356cbadfcdd889d5eab
Marcel Blasius <blasius.marcel@gmail.com>
2026-01-31 14:13:05 +0100

    added chaser enemy with hit flash and knockback

--- spectra/scripts/player.gd
+++ spectra/scripts/player.gd
@@ -1,19 +1,27 @@
 extends CharacterBody2D
 
+@export var health = 3
 @export var speed = 400
 @export var bullet_scene: PackedScene 
 @export var fire_rate: float = 0.5 # seconds
 @onready var shoot_timer = $ShootTimer
+@onready var invincivility_Timer = $InvincibilityTimer
 @onready var sprite = $Node2D/Sprite2D
 @onready var node2d = $Node2D
 @onready var anim_player : AnimationPlayer = $Node2D/AnimationPlayer
+var knockback_velocity: Vector2 = Vector2.ZERO
 
-func get_movement_direction():
+func get_movement_direction() -> Vector2:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	return input_direction * speed
 
 func _physics_process(_delta: float):
-	get_movement_direction()
+	var move_velocity = get_movement_direction()
+	
+	velocity = move_velocity + knockback_velocity
+	
+	knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, 1500 * _delta)
+	
 	move_and_slide()
 	
 	if shoot_timer.is_stopped():
@@ -58,3 +66,23 @@ func spawn_bullet(dir: Vector2, offset: Vector2):
 	bullet.global_position = global_position + offset
 	bullet.direction = dir
 	get_tree().root.add_child(bullet) 
+	
+
+func flash_hit():
+	sprite.material.set_shader_parameter("active", true)
+	await get_tree().create_timer(0.1).timeout
+	sprite.material.set_shader_parameter("active", false)
+	
+func take_damage(amount: int):
+	if !invincivility_Timer.is_stopped(): return
+	
+	health -= amount
+	flash_hit()
+	invincivility_Timer.start()
+	
+	if health == 0:
+		queue_free()
+		
+func apply_knockback(source_position: Vector2, force: float):
+	var push_dir = (global_position - source_position).normalized()
+	knockback_velocity = push_dir * force
