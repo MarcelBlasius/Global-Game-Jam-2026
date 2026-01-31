fc3a06a2118bf43712b0614290aec426eb06f9a1
CappedMonke <schuergersilasdev@gmail.com>
2026-02-01 00:28:41 +0100

    implement settings

--- spectra/scripts/main_menu.gd
+++ spectra/scripts/main_menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
+@onready var settings_menu: SettingsMenu = $SettingsMenu
+
+func _ready() -> void:
+	settings_menu.close()
+
 func _on_button_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
 
 func _on_button_settings_pressed() -> void:
-	pass
+	settings_menu.open()
 
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
---
+++ spectra/scripts/settings_menu.gd
@@ -0,0 +1,25 @@
+class_name SettingsMenu
+extends Control
+
+@onready var volume_slider: HSlider = $Window/MarginContainer/VBoxContainer/HBoxContainer/VolumeSlider
+@onready var window: MarginContainer = $Window
+
+func _ready() -> void:
+	volume_slider.value = Settings.volume
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("close_menu") && visible:
+		close()
+		get_viewport().set_input_as_handled()
+
+func open() -> void:
+	show()
+
+func close() -> void:
+	hide()
+
+func _on_close_button_pressed() -> void:
+	close()
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	Settings.volume = value
