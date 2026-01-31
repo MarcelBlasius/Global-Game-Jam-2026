30674ca8c598e18d135a2e2ef36a54f5b494e0a8
CappedMonke <schuergersilasdev@gmail.com>
2026-01-31 22:48:34 +0100

    puase game

--- spectra/scripts/game_menu.gd
+++ spectra/scripts/game_menu.gd
@@ -7,9 +7,11 @@ func _ready() -> void:
 
 func open() -> void:
 	window.show()
+	get_tree().paused = true
 
 func close() -> void:
 	window.hide()
+	get_tree().paused = false
 
 func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("open_menu") && !window.visible):
@@ -21,10 +23,12 @@ func _on_button_continue_pressed() -> void:
 	close()
 
 func _on_button_restart_pressed() -> void:
+	close()
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
 
 func _on_button_settings_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_button_exit_pressed() -> void:
+	close()
 	get_tree().change_scene_to_file("res://scenes/silas/main_menu.tscn")
