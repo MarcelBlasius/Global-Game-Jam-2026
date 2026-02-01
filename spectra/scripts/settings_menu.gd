11d45f3533181c3425661babfbd5e92b2b966dfb
Marcel Blasius <blasius.marcel@gmail.com>
2026-02-01 15:11:39 +0100

    fix volume slider

--- spectra/scripts/settings_menu.gd
+++ spectra/scripts/settings_menu.gd
@@ -5,8 +5,8 @@ extends Control
 @onready var window: MarginContainer = $Window
 
 func _ready() -> void:
-	#if (volume_slider != null):
-		#volume_slider.value = Settings.volume
+	if (volume_slider != null):
+		volume_slider.value = 50
 	pass
 
 func _input(event: InputEvent) -> void:
@@ -24,4 +24,20 @@ func _on_close_button_pressed() -> void:
 	close()
 
 func _on_volume_slider_value_changed(value: float) -> void:
-	Settings.volume = value
+	var bus_index = AudioServer.get_bus_index("Master")
+	
+	if value <= 0:
+		AudioServer.set_bus_mute(bus_index, true)
+	else:
+		AudioServer.set_bus_mute(bus_index, false)
+		# formula: (value - 50) * (target_db / 50_units)
+		# (100 - 50) * 0.24 = 12 dB
+		# (50 - 50) * 0.24 = 0 dB
+		# (1 - 50) * 0.24 = -11.76 dB
+		
+		var db_value = (value - 50) * 0.24
+		
+		if value < 50:
+			db_value = remap(value, 0, 50, -40, 0)
+			
+		AudioServer.set_bus_volume_db(bus_index, db_value)
