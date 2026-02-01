17fe714bc7a239c5faa6980fae6f79035f2eb5c1
CappedMonke <schuergersilasdev@gmail.com>
2026-02-01 12:58:33 +0100

    fixed settings/game menu bug

--- spectra/scripts/game_over_menu.gd
+++ spectra/scripts/game_over_menu.gd
@@ -7,22 +7,28 @@ extends Control
 
 var lock_input := true
 
+func _ready() -> void:
+	focus_mode = Control.FOCUS_ALL
+
 func open() -> void:
 	fade.fade_in(2.0, game_over_screen_texture)
 
 func _on_fade_fade_in_finished() -> void:
 	get_tree().paused = true
 	lock_input = false
+	grab_focus()
 
-func _input(event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if lock_input:
 		return
 
 	if event.is_pressed() and not event.is_echo():
+		if event is InputEventMouseMotion:
+			return
+			
 		_handle_any_button_pressed()
 
 func _handle_any_button_pressed() -> void:
 	lock_input = true
 	get_tree().paused = false
-	fade.fade_out(1.0)
 	get_tree().change_scene_to_file("res://scenes/silas/main_menu.tscn")
